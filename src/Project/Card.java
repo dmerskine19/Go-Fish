4d1e8a8bd3fdd0e628fcf9edef8d36544905372c
dmerskine19 <Dmerskine19@gmail.com>
2022-11-01 18:43:46 -0400

    Create Card.java

---
+++ src/Project/Card.java
@@ -0,0 +1,32 @@
+package project;
+
+public abstract class Card 
+{
+   private String suit;
+   private int value;
+    
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        
+       // CardGoFish card = new CardGoFish();
+        this.suit = suit;
+        // System.out.println(this.suit);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+        // System.out.println(this.value);
+    }
+    
+    @Override
+    public abstract String toString();
+    
+}
