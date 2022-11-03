f48354d864bcc068452154c216fcb56a429c2db2
ShawnC24 <69652935+ShawnC24@users.noreply.github.com>
2022-11-02 21:54:37 -0400

    Add files via upload

---
+++ src/Project/GoFish.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class GoFish
+{
+    public static Scanner input = new Scanner(System.in);
+    private Deck stock;
+    private Deck userHand;
+    private Deck cpHand;
+    private boolean win;
+    private int userBooks, cpBooks;
+
+    public static void main(String[]args)
+    {
+
+    }
+}
