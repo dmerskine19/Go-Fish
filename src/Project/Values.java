0eb78dee6c9c89287a2b96b8fc1631c411437fbd
ShawnC24 <69652935+ShawnC24@users.noreply.github.com>
2022-11-07 23:21:21 -0500

    Values.Java

---
+++ src/Project/Values.java
@@ -0,0 +1,20 @@
+public class Values {
+
+    enum values {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        Jacker,
+        ;
+
+    }
+}
