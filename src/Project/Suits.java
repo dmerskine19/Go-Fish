31422541bba9e7dac40e63d63fd408d4dfbb4914
ShawnC24 <69652935+ShawnC24@users.noreply.github.com>
2022-11-07 22:19:59 -0500

    Suits.Java

---
+++ src/Project/Suits.java
@@ -0,0 +1,13 @@
+public class Suits {
+    enum suits {
+        Clubs,
+        Spades,
+        Diamonds,
+        Hearts,
+    }
+
+    public static void main(String[] args) {
+        suits myVar = suits.Clubs;
+
+    }
+}
