38aa5b54379085ecdcb88179d9d9ba709e52ed30
hoangnguyen56 <112410315+hoangnguyen56@users.noreply.github.com>
2022-12-08 12:03:30 -0500

    Add files via upload

---
+++ out/production/Go-Fish/project/Test.java
@@ -0,0 +1,47 @@
+package project;
+
+import org.junit.*;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+public class Test {
+    Random rand = new Random();
+    @org.junit.Test
+    public void testPlayerStatic(){
+        Player.setPlayerID("Nhi Nguyen");
+        assertEquals("Nhi Nguyen", Player.getPlayerID());
+        assertNotEquals("Jaiden", Player.getPlayerID());
+
+    }
+    @org.junit.Test
+    public void testPlayerConstruct(){
+        Player.setPlayerID("");
+        Player NhiNguyen = new Player("Nhi Nguyen");
+        assertEquals("Nhi Nguyen",Player.getPlayerID());
+    }
+    @org.junit.Test
+    public void testSetCard(){
+        SetOfCards setCard = new SetOfCards();
+        assertEquals(0,setCard.countSet());
+        setCard.addSetCount();
+        assertEquals(1,setCard.countSet());
+        int a  = rand.nextInt(100);
+        for(int i=0;i<a;i++){
+            setCard.addSetCount();
+            assertEquals(i+2,setCard.countSet());
+        }
+    }
+    @org.junit.Test
+    public void testGroupOfCards(){
+        GroupOfCards group = new GroupOfCards(10);
+        assertEquals(10,group.getSize());
+    }
+    @org.junit.Test
+    public void testNewPlayer(){
+        NewPlayer a = new NewPlayer("OK");
+        assertEquals("OK",Player.getPlayerID());
+    }
+}
